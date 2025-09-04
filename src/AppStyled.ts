bca920737c696b74ef3a0565fa6205bae3839648
Oleksandr-Berd <alex.berd86@gmail.com>
2025-09-04 08:30:54 +0300

    initial commit

---
+++ src/AppStyled.ts
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+
+export const AppCon = styled.div`
+min-width: 325px;
+
+@media (min-width: 1280px){
+    max-width: 1280px;
+
+    margin-left: auto;
+    margin-right: auto;
+}
+`
